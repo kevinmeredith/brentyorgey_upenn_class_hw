abdbf5cd80e319d476d3ae1ef31005a7e119f865
kman007us <kevin.m.meredith@gmail.com>
2014-09-27 21:19:05 -0400

    answered question 2

--- Homework5/Calc.hs
+++ Homework5/Calc.hs
@@ -2,8 +2,12 @@
 module Calc where
 
 import ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit x)     = x
 eval (Add e1 e2) = eval e1 + eval e2
 eval (Mul e1 e2) = eval e1 * eval e2	
+
+evalStr :: String -> Maybe Integer
+evalStr xs = parseExp Lit Add Mul xs >>= (\x -> Just $ eval x)
