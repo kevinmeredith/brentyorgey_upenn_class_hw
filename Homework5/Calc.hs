20bba0ea7f4bdfc07d3d0518dc0df76ab9856050
kman007us <kevin.m.meredith@gmail.com>
2014-09-27 22:38:52 -0400

    started problem 3

--- Homework5/Calc.hs
+++ Homework5/Calc.hs
@@ -11,3 +11,13 @@ eval (Mul e1 e2) = eval e1 * eval e2
 
 evalStr :: String -> Maybe Integer
 evalStr xs = parseExp Lit Add Mul xs >>= (\x -> Just $ eval x)
+
+class Expr a where
+	lit :: Integer -> a
+	add :: a -> a -> a
+	mul :: a -> a -> a
+
+instance Expr ExprT where
+	lit x     = Lit x
+	add e1 e2 = Add e1 e2
+	mul e1 e2 = Mul e1 e2
