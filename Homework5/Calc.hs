57d30b86ee8c08d2ecd799fbbbe572f844370ddd
Kevin Meredith <kevin.meredith@mckesson.com>
2014-09-28 22:35:08 -0400

    continued working on problem 4...

--- Homework5/Calc.hs
+++ Homework5/Calc.hs
@@ -12,12 +12,33 @@ eval (Mul e1 e2) = eval e1 * eval e2
 evalStr :: String -> Maybe Integer
 evalStr xs = parseExp Lit Add Mul xs >>= (\x -> Just $ eval x)
 
-class Expr a where
-	lit :: Integer -> a
-	add :: a -> a -> a
-	mul :: a -> a -> a
+reify :: ExprT -> ExprT
+reify = id
 
-instance Expr ExprT where
-	lit x     = Lit x
-	add e1 e2 = Add e1 e2
-	mul e1 e2 = Mul e1 e2
+class Expr a where
+	lit :: Integer        -> a
+	add :: ExprT -> ExprT -> a
+	mul :: ExprT -> ExprT -> a
+
+instance Expr Integer where
+	lit x     = x
+	add e1 e2 = eval e1 + eval e2 
+	mul e1 e2 = eval e1 * eval e2
+
+instance Expr Bool where
+	lit x     = x > 0  
+	add e1 e2 = eval e1 > 0 || eval e2 > 0 
+	mul e1 e2 = eval e1 > 0 && eval e2 > 0
+
+instance Expr MinMax where
+	lit x     = MinMax x
+	add e1 e2 = MinMax $ max (eval e1) (eval e2)
+	mul e1 e2 = MinMax $ min (eval e1) (eval e2)
+
+instance Expr Mod7 where
+	lit x     = Mod7 $ x `mod` 7 
+	add e1 e2 = Mod7 $ (eval e1 + eval e2) `mod` 7 
+	mul e1 e2 = Mod7 $ (eval e1 * eval e2) `mod` 7
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7   = Mod7 Integer deriving (Eq, Show)
