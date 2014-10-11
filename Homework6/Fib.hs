7c6f96f8dd3ec92a3419324a450b2bb2d3c6332f
kevinmeredith <kevin.m.meredith@gmail.com>
2014-10-11 02:13:34 +0000

    sped up Fibonacci

--- Homework6/Fib.hs
+++ Homework6/Fib.hs
@@ -10,12 +10,21 @@ fib n
 fibs :: [Integer]
 fibs = Prelude.map fib [0..]
 
+fibMemo :: Integer -> Map Integer Integer -> Integer
+fibMemo n map
+ | n == 0    = 0
+ | n == 1    = 1
+ | otherwise = case maybeMember of Just x  -> x
+                                   Nothing -> calced
+               where maybeMember = M.lookup n map
+                     calced      = fibMemo (n-1) map + fibMemo (n-2) map
+
 -- improved fibs using memoized (of already calculated fibonacci values)
 fibsImproved :: [Integer]
 fibsImproved = fibsImproved' 0 M.empty
 
 fibsImproved' :: Integer -> Map Integer Integer -> [Integer]
-fibsImproved' n memoized  = case maybeMember of Just x  -> x : fibsImproved' (n+1) memoized
+fibsImproved' n memoized  = case maybeMember of Just x  -> x      : fibsImproved' (n+1) memoized
                                                 Nothing -> calced : fibsImproved' (n+1) (M.insert n calced $ memoized)
                             where maybeMember = M.lookup n memoized
-                                  calced      = fib n
+                                  calced      = fibMemo n memoized
