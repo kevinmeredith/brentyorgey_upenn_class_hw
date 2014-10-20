18c470386806d23e2859c1edad12fba7c4abd8c8
Kevin Meredith <kevin.m.meredith@gmail.com>
2014-10-19 21:07:01 -0400

    added functions for examining the pattern - looks like every other is 0, then 1, then 2, then ...

--- Homework6/RealFracWork.hs
+++ Homework6/RealFracWork.hs
@@ -8,6 +8,19 @@ largestPowerDivBy2 x = largestPowerDivBy2' x 0
                   where largestPowerDivBy2' num acc = if num `rem` (2^(acc+1)) == 0 then largestPowerDivBy2' num (acc+1)
            	                                          else (2^acc)
 
+-- index starting at 0
+getOddIndexedElems :: [a] -> [a]
+getOddIndexedElems =  map snd . filter (\x -> odd $ fst x) . (zip [0..])
+
+infiniteF :: [Integer]
+infiniteF = map f [1..] 
+
+getEveryFourth :: [a] -> [a]
+getEveryFourth = map snd . filter (\x -> fst x `mod` 4 == 0) . zip [1..] 
+
+getEveryOther :: [a] -> [a]
+getEveryOther = map snd . filter (\x -> even $ fst x) . zip [1..] 
+
 --[1,2,3,4,5,6,7]
 
 --[0,1,0,2,0,1,0, ]
