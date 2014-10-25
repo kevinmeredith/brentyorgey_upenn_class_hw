bbadadeade8513edd9e8acf6ca36c487da1ac3ea
Kevin Meredith <kevin.m.meredith@gmail.com>
2014-10-24 23:51:43 -0400

    fixed interleave per 'mjolka's help on Code Review chat.

--- Homework6/StreamRefined.hs
+++ Homework6/StreamRefined.hs
@@ -7,7 +7,7 @@ data Stream a = Cons a (Stream a)
 streamToList :: Stream a -> [a]
 streamToList (Cons x str) = x : streamToList str
 
--- Show 30 items of the list rather than infinitity
+-- Show 20 items of the list rather than infinitity
 instance Show a => Show (Stream a) where
 	show stream = show $ go stream 1 
 	                     where go (Cons x rest) count 
@@ -17,8 +17,11 @@ instance Show a => Show (Stream a) where
 streamRepeat :: a -> Stream a
 streamRepeat x = Cons x (streamRepeat x)	                            
 
+--interleave :: Stream a -> Stream a -> Stream a
+--interleave (Cons x strX) (Cons y strY) = Cons x (Cons y (interleave strX strY))
+
 interleave :: Stream a -> Stream a -> Stream a
-interleave (Cons x strX) (Cons y strY) = Cons x (Cons y (interleave strX strY))
+interleave (Cons x xs) ys = Cons x (interleave ys xs)
 
 ruler :: Stream Integer
 ruler = ruler' 0
