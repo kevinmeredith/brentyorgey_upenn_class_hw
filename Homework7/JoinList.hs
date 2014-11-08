0b7c9d4e795fd2d503c7a8d4e751f21f630c3cf3
Kevin Meredith <kevin.m.meredith@gmail.com>
2014-11-07 21:36:48 -0500

    re-implemented jIndex - testing more.

--- Homework7/JoinList.hs
+++ Homework7/JoinList.hs
@@ -28,20 +28,26 @@ jl2 :: JoinList (Product Integer) String
 jl2 = Append (Product 50) (Single (Product 25) "bippy") (Single (Product 2) "baz")
 
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
-indexJ _ Empty          = Nothing
-indexJ i (Single _ x)
-  | i == 0              = Just x
-  | otherwise           = Nothing
-indexJ i (Append j left right) 
-  | 
-  where f = getSize . size . tag
-        g = getSize . size
+indexJ _ Empty                 = Nothing
+indexJ i (Single _ x) 
+  | i == 0    = Just x
+  | otherwise = Nothing
+indexJ i (Append _ left right)
+  | leftHt > i  = indexJ i left
+  | (rightHt + leftHt) > i = indexJ (i-leftHt) right
+  | otherwise    = Nothing
+  where leftHt = (getSize. size . tag) left
+        rightHt = (getSize. size . tag) right
+
 
 jlIndex1 :: JoinList Size String
 jlIndex1 = Append (Size 2) (Single (Size 1) "foo") (Single (Size 1) "bar")
 
 jlIndex2 :: JoinList Size String
 jlIndex2 = Append (Size 3) (Single (Size 1) "foo") (Append (Size 2) (Single (Size 1) "bar") (Single (Size 1) "baz"))
+
+jlIndex3 :: JoinList Size String
+jlIndex3 = Append (Size 4) Empty
 
 --dropJ ::(Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
 --dropJ _ Empty = Empty
@@ -52,3 +58,16 @@ jlIndex2 = Append (Size 3) (Single (Size 1) "foo") (Append (Size 2) (Single (Siz
 --  | i == 0                           = append
 --  | (getSize . size . tag) left >  i = indexJ i left
 --  | otherwise                        = indexJ i right
+
+(!!?) :: [a] -> Int -> Maybe a
+[]     !!? _         = Nothing
+_      !!? i | i < 0 = Nothing
+(x:_)  !!? 0         = Just x
+(_:xs) !!? i         = xs !!? (i-1)
+
+jToList :: JoinList m a -> [a]
+jToList Empty          = []
+jToList (Single _ x)   = [x]
+jToList (Append _ x y) = jToList x ++ jToList y
+
+-- testing via QuickCheck
