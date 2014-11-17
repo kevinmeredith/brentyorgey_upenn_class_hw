a0f295ea9be38638e7759d77f788ceadece4bf80
Kevin Meredith <kevin.m.meredith@gmail.com>
2014-11-16 22:43:58 -0500

    implemented functor instance for ITree.

--- Typeclassopedia/FunctorEx.hs
+++ Typeclassopedia/FunctorEx.hs
@@ -20,13 +20,25 @@ data Pair a = P (a, a) deriving Show
 instance Functor (Pair) where
     fmap f (P (x, y)) = P (f x, f y)
 
-data ITree a = Leaf (Int -> a)
-             | Node [ITree a]
+data ITree a = Leaf (Int -> a) | Node [ITree a]
 
 -- (a -> b) -> (ITree a) -> (ITree b)    
 
 instance Functor (ITree) where
-    fmap f (Leaf g)  = Leaf $ f . g
-    fmap f (Node xs) = Node $ go xs
-                       where go (y:ys) = f y : go ys
-                             go []     = []
+	fmap = iTreeMap
+
+iTreeMap :: (a -> b) -> ITree a -> ITree b
+iTreeMap f (Leaf x)  = Leaf $ fmap f x
+iTreeMap f (Node xs) = Node $ map (iTreeMap f) xs
+
+foldTree' :: ((Int -> a) -> b) -> ([b] -> b) -> ITree a -> b
+foldTree' f _ (Leaf x)  = f x
+foldTree' f g (Node xs) = g $ map (foldTree' f g) xs
+
+--foldTree' :: (a -> [b] -> b) -> ITree a -> b
+--foldTree' f (Leaf x)  = f x []
+--foldTree' f (Node xs) = f _ $ map (foldTree f) xs  
+
+-- #4
+-- Give an example of a type of kind * -> * which cannot be made an 
+-- instance of Functor (without using undefined).
