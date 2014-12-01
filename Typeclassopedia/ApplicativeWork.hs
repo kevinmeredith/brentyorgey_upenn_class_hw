f1e1b232817d367000c0e321eb96c788b09ddc15
Kevin Meredith <Kevin.m.meredith@gmail.com>
2014-11-30 22:14:14 -0500

    Implemented exercises for Applicative.

--- Typeclassopedia/ApplicativeWork.hs
+++ Typeclassopedia/ApplicativeWork.hs
@@ -21,11 +21,35 @@ import Control.Applicative
 
 -- equal
 
--- Make the Applicative Instance for Maybe
+-- Exercise #1: Make the Applicative Instance for Maybe
+data Option a = Some a 
+                | None deriving Show
 
-data Option a = Some a | None deriving Show
+instance Functor Option where
+   fmap _ None     = None
+   fmap f (Some x) = Some (f x)
 
 instance Applicative Option where
    pure x                = Some x
    (Some f) <*> (Some x) = Some (f x) 
    _ <*> _               = None  
+
+-- Exercise #2: Determine the correct definition of pure for the ZipList instance of Applicative—there 
+-- is only one implementation that satisfies the law relating pure and (<*>).   
+newtype ZList a = ZList { getZList :: [a] } deriving Show
+ 
+instance Functor ZList where
+	fmap f (ZList xs) = ZList (fmap f xs)
+
+instance Applicative ZList where
+  pure x                    = ZList (repeat x)
+  (ZList gs) <*> (ZList xs) = ZList (zipWith ($) gs xs)
+
+class Functor f => Monoidal f where
+  unit :: f ()
+  (**) :: f a -> f b -> f (a,b)
+
+-- Exercise: Implement pure and (<*>) in terms of unit and (**), and vice versa.
+
+--class Functor f => MyApplicative f where
+--	pure 
