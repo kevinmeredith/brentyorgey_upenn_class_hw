3d4daddbe820902c456e2c9211280ee6f1b3b5eb
Kevin Meredith <Kevin.m.meredith@gmail.com>
2015-01-01 15:19:58 -0500

    implemented mapA.

--- Homework11/ApplicativeWork.hs
+++ Homework11/ApplicativeWork.hs
@@ -5,10 +5,12 @@ import Control.Applicative
 (*>) :: Applicative f => f a -> f b -> f b
 (*>) _ f = f
 
---mapA :: Applicative f => (a -> f b) -> ([a] -> f [b])
---mapA 
+mapA :: Applicative f => (a -> f b) -> ([a] -> f [b])
+mapA _ []     = pure []
+mapA f (x:xs) = (:) <$> f x <*> mapA f xs
 
+-- see codereview for improvement
+-- http://codereview.stackexchange.com/questions/75397/implementing-sequencea
 sequenceA :: Applicative f => [f a] -> f [a]
 sequenceA []     = pure []
 sequenceA (x:xs) = (++) <$> (fmap (\y -> [y]) x) <*> sequenceA xs
-
