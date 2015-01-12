4ab99a7d96db7a0c8e10695a94d58166a3063f23
Kevin Meredith <kevinmeredith@Kevins-MacBook-Pro.local>
2015-01-11 23:51:12 -0500

    trying to implement Parser for S Expr - need to read StackOverflow answer(s) first on how an SExpr gets parsed.

--- Homework11/SExpr.hs
+++ Homework11/SExpr.hs
@@ -48,3 +48,12 @@ parseAtom :: Parser Atom
 parseAtom = alt n i
    where n = (\_ _ z -> N z) <$> (char 'N') <*> spaces <*> posInt
          i = (\_ _ z -> I z) <$> (char 'I') <*> spaces <*> ident
+
+parseAAtom :: Parser SExpr
+parseAAtom = fmap (\x -> A x) parseAtom         
+
+parseComb :: Parser SExpr
+parseComb = (\_ x _ -> (: x)) <$> ((zeroOrMore spaces) *> (char '(')) <*> (alt parseAAtom parseComb) <*> ((zeroOrMore spaces) *> (char ')'))
+
+--parseSExpr :: Parser SExpr
+--parseSExpr = alt parseAAtom parseComb
