1bb304d31ddc9e8e77a0c0438788e30cb50660e7
Kevin Meredith <kevin.m.meredith@gmail.com>
2015-01-21 00:11:02 -0500

    implemented 'compete' for comparing List of Dice Rolls. Next step is to finish implementing random.

--- Homework12/Risk.hs
+++ Homework12/Risk.hs
@@ -5,6 +5,7 @@
 module Risk where
 
 import Control.Monad.Random
+import Data.List
 
 ------------------------------------------------------------
 -- Die values
@@ -44,9 +45,39 @@ twoInts gen = let (one, gen')  = random gen
               in ((abs one, abs two), gen'')
 
 --fightSingleRound :: (Rand StdGen Battlefield) -> Battlefield
---fightSingleRound = 
+--fightSingleRound rand_bf = 
+--	where bf = evalRand rand_bf
+--		  (as, ds) = (attackers bf, defenders bf)
 
-getDie :: Int -> [DieValue]
-getDie n 
+type AttackersDice = [DieValue]
+type DefendersDice = [DieValue]
+
+type Deaths = (Army, Army)
+
+attacker_dies :: (Int, Int)
+attacker_dies = (1, 0)
+
+defender_dies :: (Int, Int)
+defender_dies = (0, 1)
+
+compete :: AttackersDice -> DefendersDice -> Deaths
+compete as_dice ds_dice = (attacker_deaths, defender_deaths)
+    where attacker_deaths  = foldr (\x acc -> fst x + acc) 0 results
+          defender_deaths  = foldr (\x acc -> snd x + acc) 0 results
+          results          = map (\(x, y) -> if x > y then defender_dies else attacker_dies) $ zip as ds -- defender wins on EQ or GT
+          as               = reverse . sort . map (unDV) $ as_dice
+          ds               = reverse . sort . map (unDV) $ ds_dice
+
+rollDieN :: Int -> [DieValue]
+rollDieN n 
   | n < 0     = []
-  | otherwise = replicate n $ evalRand die (mkStdGen 55)
+  | otherwise = evalRand die (mkStdGen n) : rollDieN (n-1) 
+
+-- if the Attacker rolls a 1, 2, and 3 & the Defender rolls a 2 and 3, then
+-- the Attacker should lose 2 soldiers, and the Defender none.
+competeTest :: Bool
+competeTest = (compete as_dice ds_dice) == expected
+  where
+  	expected = (2, 0)
+  	as_dice  = [DV 1, DV 2, DV 3]
+  	ds_dice  = [DV 2, DV 3]
