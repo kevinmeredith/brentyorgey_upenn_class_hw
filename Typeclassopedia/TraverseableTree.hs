5087eafa72d97022dc040c23a98e6c48d79c19b3
Kevin Meredith <Kevin.m.meredith@gmail.com>
2016-01-02 00:39:06 -0500

    Foldable and Traverseable instances of Tree.

---
+++ Typeclassopedia/TraverseableTree.hs
@@ -0,0 +1,25 @@
+import Data.Traversable as T
+import Control.Applicative
+
+data Tree a = Empty | Leaf a | Node (Tree a) a (Tree a) deriving Show
+
+instance (Functor Tree) where
+	fmap _ Empty        = Empty
+	fmap f (Leaf x)     = Leaf (f x)
+	fmap f (Node l x r) = Node (fmap f l) (f x) (fmap f r)
+
+--traverse
+--  :: (Traversable t, Control.Applicative.Applicative f) =>
+--     (a -> f b) -> t a -> f (t b)
+
+instance (Foldable Tree) where
+  foldr f acc Empty        = acc
+  foldr f acc (Leaf x)     = f x acc
+  foldr f acc (Node l x r) = foldr f (foldr f (f x acc) r) l
+
+  -- (a -> b -> b) -> b -> t a -> b 
+
+instance (Traversable Tree) where
+	traverse _ Empty        = pure Empty
+	traverse f (Leaf x)     = Leaf <$> f x
+	traverse f (Node l x r) = _
