5371c0de4415fc3eeb0b6c47d35f52761f805625
Kevin Meredith <kevin.m.meredith@gmail.com>
2016-07-26 21:54:32 -0400

    Fix filterM.

--- Etc/FilterM.hs
+++ Etc/FilterM.hs
@@ -1,9 +1,4 @@
-filterM' :: Monad m => (a -> m Bool) -> [a] -> m [a]
-filterM' _ []     = return []
-filterM' p (x:xs) =
-	let rest = filterM' p xs in 
-	do b <- p x
-	   if (b) then rest >>= (\a -> return (x : a)) else rest
-
--- filterM' (const $ [True, False]) [1,2,3]
--- [[1,2,3],[1,2],[1,3],[1],[2,3],[2],[3],[]]
+filterM' :: Applicative m => (a -> m Bool) -> [a] -> m [a]
+filterM' _ []       = pure []
+filterM' f (x : xs) = let first = pure (\bool -> if (bool) then [x] else []) <*> (f x)
+                      in (fmap (++) first) <*> filterM' f xs
